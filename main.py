88d54262b7848b26b3f479b24c7cb3506df34fcc
Matthew Freestone <matthewfreestone1593@gmail.com>
2022-12-13 17:41:35 -0600

    added mongodb

---
+++ main.py
@@ -0,0 +1,63 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+from src.utils import room_num_to_id
+from src.mongo import *
+
+app = Flask(__name__)
+URL = "https://spider.eng.auburn.edu/makerspace/ajax-multi.php"
+
+@app.route('/')
+def index():
+    return render_template('index.html', reservations=get_all_entries())
+
+@app.route('/test_endpoint', methods=['POST'])
+def test_endpoint():
+    return dict(request.form)
+
+@app.route('/reserve', methods=['POST'])
+def reserve():
+    room_num = request.form['room']
+    object_num = room_num_to_id(room_num)
+    username = request.form['username']
+    start_time = request.form['start']
+    end_time = request.form['end']
+    date = request.form['date']
+    post_data = {
+        "object": object_num,
+        "username": username,
+        "StartTime": start_time,
+        "EndTime": end_time,
+        "Date": date,
+    }
+    r = requests.post(URL, data=post_data)
+    res = r.json()
+    if res['error'] == False:
+        post_data['appt_id'] = res['appt_id']
+        post_data['room'] = room_num
+        write_to_db(post_data)
+        return redirect(url_for('index'))
+    else:
+        return {'error': res['errorMsg']}
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    room_num = room_num_to_id(request.form['room'])
+    appt_id = request.form['appt_id']
+    post_data = {
+        "object": room_num,
+        "appt_id": appt_id,
+        "delete": 1
+    }
+    r = requests.post(URL, data=post_data)
+    res = r.json()
+    if res['error'] == False:
+        delete_from_db(appt_id)
+        return redirect(url_for('index'))
+    else:
+        return {'error': res['errorMsg']}
+
+if __name__ == '__main__':
+    app.run()
---
+++ src/mongo.py
@@ -0,0 +1,26 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+
+mongo_url = os.getenv("MONGO_URL")
+
+client = MongoClient(mongo_url, server_api=ServerApi('1'))
+env = os.getenv("MONGO_ENV")
+db = client[env]
+
+def write_to_db(data):
+    db.appt.insert_one(data)
+
+def delete_from_db(appt_id: str):
+    db.appt.delete_one({"appt_id": appt_id})
+
+def get_all_entries():
+    return db.appt.find()
+
+# read all entries
+def main():
+    for entry in db.appt.find():
+        print(entry)
+
+if __name__ == '__main__':
+    main()
